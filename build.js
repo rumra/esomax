543d2a69338f1b985126d2aaced67162be7ed5c5
Rumra <rumra93@gmail.com>
2016-10-16 20:34:04 +0600

    add scss support

--- build.js
+++ build.js
@@ -1,30 +1,48 @@
-var Metalsmith = require("metalsmith");
-var layouts = require("metalsmith-layouts");
-var handlebars = require("handlebars");
-var serve = require("metalsmith-serve");
-var watch = require("metalsmith-watch");
+var Metalsmith = require('metalsmith');
+var layouts = require('metalsmith-layouts');
+var assets = require('metalsmith-assets');
+var handlebars = require('handlebars');
+var serve = require('metalsmith-serve');
+var watch = require('metalsmith-watch');
+var sass = require('metalsmith-sass');
 
 var handlebarsLayouts = require('handlebars-layouts');
 
-handlebars.registerHelper(handlebarsLayouts(handlebars));
 handlebarsLayouts.register(handlebars);
 
 
 Metalsmith(__dirname)
-  .source("app/src")
+  .source('app/src')
+  .use(
+    sass({
+      outputStyle: 'compressed',
+      sourceMap: true,
+      sourceMapContents: true
+    })
+  )
   .use(
     layouts({
       engine: 'handlebars',
       directory: 'app/layouts',
-      partials: 'app/layouts/layout'
+      partials: 'app/partials'
     })
   )
-  .use(serve())
+  // .use(
+  //   assets({
+  //     source: './app/assets',
+  //     destination: './assets'
+  //   })
+  // )
+  .use(serve({
+    port: 8080,
+    verbose: true
+  }))
   .use(
     watch({
       paths: {
-        "${source}/**/*": true,
-        "app/layouts/**/*": "**/*.hbs",
+        '${source}/**/*': true,
+        'app/layouts/**/*': '**/*.hbs',
+        'app/partials/**/*': "**/*.hbs",
       },
       livereload: true,
     })
@@ -35,7 +53,7 @@ Metalsmith(__dirname)
 /*     manipulate files       */
 /*     *******************    */
 
-  .destination("docs")
+  .destination('docs')
   .build(function(err) {
     if (err) throw err;
     console.log('Build finished!');
